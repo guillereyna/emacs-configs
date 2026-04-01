dd4553d6039189da5c81fe5139fcf4909413ee94
greyna_meli <guillermo.reyna@mercadolibre.com>
2026-04-01 13:17:59 -0300

    better file structure

--- init.el
+++ init.el
@@ -1,8 +1,18 @@
 ;;; -*- lexical-binding: t -*-
 
-;; track recent files and minibuffer history
+;;; bootstrap ----------------------------------------------------------------
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(setq use-package-always-ensure t)
+
+;;; built-in behaviour -------------------------------------------------------
+
 (recentf-mode 1)
 (savehist-mode 1)
+(delete-selection-mode 1)
+(electric-pair-mode 1)
+(global-auto-revert-mode 1)
 
 ;; tidy up auto-save, backup, and lock files
 (dolist (dir (list (concat user-emacs-directory "backups")
@@ -13,12 +23,14 @@
       auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory "auto-saves/") t))
       lock-file-name-transforms `((".*" ,(concat user-emacs-directory "locks/") t)))
 
-;; package repositories and initialization
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-(setq use-package-always-ensure t)
+(setq global-auto-revert-non-file-buffers t
+      auto-revert-remote-files nil
+      scroll-conservatively 101
+      scroll-margin 3
+      read-process-output-max (* 1024 1024))
 
-;; visual style
+;;; appearance ---------------------------------------------------------------
+
 (column-number-mode 1)
 (when (find-font (font-spec :name "Iosevka"))
   (set-frame-font "Iosevka 13" nil t))
@@ -33,31 +45,48 @@
                       :foreground "#3a3a3a"
                       :background (face-background 'default)))
 
-;; line number configs
 (setq display-line-numbers-type t) ;; can be 'relative
 (global-display-line-numbers-mode 1)
-(dolist (mode '(eshell-mode-hook
-		shell-mode-hook))
+(dolist (mode '(eshell-mode-hook shell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-;; custom behaviour
-(delete-selection-mode 1)
-(electric-pair-mode 1)
-(global-auto-revert-mode 1)
-(setq global-auto-revert-non-file-buffers t
-      auto-revert-remote-files nil
-      scroll-conservatively 101
-	  scroll-margin 3
-	  read-process-output-max (* 1024 1024))
+(use-package display-fill-column-indicator
+  :ensure nil
+  :hook (prog-mode . display-fill-column-indicator-mode)
+  :custom
+  (fill-column 100)
+  (display-fill-column-indicator-character ?╎))
 
-;; custom functions
+(use-package ligature
+  :config
+  (ligature-set-ligatures 'prog-mode
+    '("<---" "<--" "<-" "->" "-->" "--->" "<->" "<-->" "<--->"
+      "==" "===" "!==" "!=" ">=" "<=" "<=>"
+      "--" "---"
+      ":=" "!!" "&&" "||"
+      "=>" ">>=" "<<=" "=/=" "<<" ">>" "<<<" ">>>"))
+  (global-ligature-mode 1))
+
+(use-package nerd-icons
+  :if (display-graphic-p)
+  :config (unless (find-font (font-spec :name "Symbols Nerd Font Mono"))
+            (nerd-icons-install-fonts t)))
+
+(use-package doom-modeline
+  :after nerd-icons
+  :config (doom-modeline-mode 1))
+
+(use-package ansi-color
+  :hook (compilation-filter . ansi-color-compilation-filter))
+
+;;; keybindings and custom functions -----------------------------------------
+
 (defun open-term-window-below ()
   (interactive)
   (split-window-below)
   (windmove-down)
   (term "/bin/zsh"))
 
-;; non-package specific bindings
 (keymap-global-set "M-S-<left>"  'windmove-left)
 (keymap-global-set "M-S-<right>" 'windmove-right)
 (keymap-global-set "M-S-<up>"    'windmove-up)
@@ -75,56 +104,7 @@
 (keymap-set minibuffer-local-map "<escape>" 'minibuffer-keyboard-quit)
 (keymap-set special-mode-map "<escape>" 'quit-window)
 
-;; displays a line at the fill column for programming modes
-(use-package display-fill-column-indicator
-  :ensure nil
-  :hook (prog-mode . display-fill-column-indicator-mode)
-  :custom
-  (fill-column 100)
-  (display-fill-column-indicator-character ?╎)
-)
-
-;; ligatures for programming modes
-(use-package ligature
-  :config
-  (ligature-set-ligatures 'prog-mode
-    '("<---" "<--" "<-" "->" "-->" "--->" "<->" "<-->" "<--->"
-      "==" "===" "!==" "!=" ">=" "<=" "<=>"
-      "--" "---"
-      ":=" "!!" "&&" "||"
-      "=>" ">>=" "<<=" "=/=" "<<" ">>" "<<<" ">>>"))
-  (global-ligature-mode 1))
-
-;; makes term mode have yank functionality
-(use-package term
-  :hook (term-mode . (lambda () (display-line-numbers-mode 0)))
-  :bind (:map term-raw-map ("C-c y" . term-paste)))
-
-
-;; makes compile mode have pretty colors
-(use-package ansi-color
-  :hook (compilation-filter . ansi-color-compilation-filter))
-
-;; go-mode is not included in the default package repositories, we need to install it manually
-(use-package go-mode
-  :commands go-mode
-  :hook (go-mode . lsp-deferred)
-  :config
-  (defun lsp-go-install-save-hooks ()
-    (add-hook 'before-save-hook #'lsp-format-buffer t t)
-    (add-hook 'before-save-hook #'lsp-organize-imports t t))
-  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks))
-
-(use-package lua-mode
-  :commands lua-mode)
-
-(use-package multiple-cursors
-  :commands (mc/mark-next-like-this-word mc/mark-previous-like-this
-             mc/mark-all-like-this mc/edit-lines)
-  :bind (("C->" . mc/mark-next-like-this)
-         ("C-<" . mc/mark-previous-like-this)
-         ("C-c m" . mc/mark-all-like-this)
-         ("C-c l" . mc/edit-lines)))
+;;; navigation and completion ------------------------------------------------
 
 (use-package which-key
   :demand t
@@ -132,14 +112,13 @@
   :custom (which-key-idle-delay 0.1)
   :config (which-key-mode))
 
-;; ivy, counsel, projectile, company and completions
 (use-package ivy
+  :demand t
   :diminish ivy-mode
   :bind (("C-s" . swiper-or-region)
-		 ("C-r" . swiper-isearch)
+         ("C-r" . swiper-isearch)
          :map ivy-minibuffer-map
          ("<escape>" . minibuffer-keyboard-quit))
-  :demand t
   :config
   (defun swiper-or-region ()
     (interactive)
@@ -150,6 +129,7 @@
   (setf (alist-get 'counsel-M-x ivy-initial-inputs-alist) ""))
 
 (use-package counsel
+  :after ivy
   :bind (("C-x b"   . counsel-switch-buffer)
          ("M-x"     . counsel-M-x)
          ("C-x C-f" . counsel-find-file)
@@ -158,12 +138,20 @@
          ("M-y"     . counsel-yank-pop)
          ("C-c r"   . counsel-compile)
          ("C-h f"   . counsel-describe-function)
-         ("C-h v"   . counsel-describe-variable))
-  :after ivy)
+         ("C-h v"   . counsel-describe-variable)))
 
 (use-package ivy-rich
   :after (ivy counsel)
   :config (ivy-rich-mode 1))
+
+(use-package company
+  :diminish company-mode
+  :bind (("C-c SPC" . company-complete)
+         :map company-active-map
+         ("<escape>" . company-abort))
+  :hook (prog-mode . company-mode))
+
+;;; project management -------------------------------------------------------
 
 (use-package projectile
   :demand t
@@ -177,28 +165,54 @@
   :after (counsel projectile)
   :config (counsel-projectile-mode 1))
 
-(use-package company
-  :diminish company-mode
-  :bind (("C-c SPC" . company-complete)
-         :map company-active-map
-         ("<escape>" . company-abort))
-  :hook (prog-mode . company-mode))
+;;; editing utilities --------------------------------------------------------
 
-;; magit <3
+(use-package multiple-cursors
+  :commands (mc/mark-next-like-this-word mc/mark-previous-like-this
+             mc/mark-all-like-this mc/edit-lines)
+  :bind (("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-c m" . mc/mark-all-like-this)
+         ("C-c l" . mc/edit-lines)))
+
+(use-package move-text
+  :bind (("M-<up>"   . move-text-up)
+         ("M-<down>" . move-text-down)))
+
+;;; terminal -----------------------------------------------------------------
+
+(use-package term
+  :hook (term-mode . (lambda () (display-line-numbers-mode 0)))
+  :bind (:map term-raw-map ("C-c y" . term-paste)))
+
+;;; vcs ----------------------------------------------------------------------
+
 (use-package magit
   :commands magit-status
   :custom (magit-diff-refine-hunk 'all)
   :config (add-hook 'after-save-hook #'magit-after-save-refresh-status t))
 
-;; LSP integration
+;;; lsp and languages --------------------------------------------------------
+
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
   :init (setq lsp-keymap-prefix "C-c C-l")
   :bind* ("C-c d" . flymake-show-buffer-diagnostics)
   :config (lsp-enable-which-key-integration 1))
 
+(use-package go-mode
+  :hook (go-mode . lsp-deferred)
+  :config
+  (defun lsp-go-install-save-hooks ()
+    (add-hook 'before-save-hook #'lsp-format-buffer t t)
+    (add-hook 'before-save-hook #'lsp-organize-imports t t))
+  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks))
 
-;; treemacs and extensions, most are native to treemacs
+(use-package lua-mode
+  :commands lua-mode)
+
+;;; file tree ----------------------------------------------------------------
+
 (use-package treemacs
   :commands treemacs
   :hook (treemacs-mode . (lambda () (display-line-numbers-mode 0)))
@@ -222,6 +236,7 @@
                 (treemacs-add-and-display-current-project)))))
 
 (use-package lsp-treemacs
+  :after (treemacs lsp-mode)
   :commands (lsp-treemacs-errors-list lsp-treemacs-symbols lsp-treemacs-references
              lsp-treemacs-call-hierarchy lsp-treemacs-implementations)
   :bind (:map lsp-mode-map
@@ -230,30 +245,14 @@
          ("C-c C-l e r" . lsp-treemacs-references)
          ("C-c C-l e c" . lsp-treemacs-call-hierarchy)
          ("C-c C-l e i" . lsp-treemacs-implementations))
-  :config (lsp-treemacs-sync-mode 1)
-  :after (treemacs lsp-mode))
+  :config (lsp-treemacs-sync-mode 1))
 
-;; required for doom-modeline
-(use-package nerd-icons
-  :if (display-graphic-p)
-  :config (unless (find-font (font-spec :name "Symbols Nerd Font Mono"))
-	    (nerd-icons-install-fonts t)))
+;;; local and custom overrides -----------------------------------------------
 
-(use-package doom-modeline
-  :after nerd-icons
-  :config (doom-modeline-mode 1))
-
-;; misc
-(use-package move-text
-  :bind (("M-<up>" . move-text-up)
-	 ("M-<down>" . move-text-down)))
-
-;; machine specific local configs, loaded second to last
 (let ((local-configs (concat user-emacs-directory "local-configs.el")))
   (when (file-exists-p local-configs)
     (load-file local-configs)))
 
-;; setup and load customization file, load last so we don't conflict package declarations
 (let ((custom-configs (concat user-emacs-directory "custom.el")))
   (unless (file-exists-p custom-configs)
     (make-empty-file custom-configs))
